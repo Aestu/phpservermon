b0ef2fc335e59587b23237330bfecb2ae058c323
Alfredo Esteban <aedelatorre@gmail.com>
2016-08-28 12:24:51 +0200

    proxy initial

--- src/includes/functions.inc.php
+++ src/includes/functions.inc.php
@@ -335,6 +335,11 @@ function psm_curl_get($href, $header = false, $body = true, $timeout = null, $ad
 
 	curl_setopt($ch, CURLOPT_URL, $href);
 
+	$proxy_url = psm_get_conf('proxy_url','');
+	if (psm_get_conf('proxy','0') === '1' && !empty($proxy_url)) {
+		curl_setopt($ch, CURLOPT_PROXY, $proxy_url);
+	}
+
 	if($add_agent) {
 		curl_setopt ($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; phpservermon/'.PSM_VERSION.'; +http://www.phpservermonitor.org)');
 	}
